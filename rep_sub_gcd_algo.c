bb4233b6c53fb2e54ed09d9fd71b76b40b4939e5
prathibha27 <42810644+prathibha27@users.noreply.github.com>
2020-02-04 11:57:02 +0530

    Create rep_sub_gcd_algo.c

---
+++ rep_sub_gcd_algo.c
@@ -0,0 +1,24 @@
+
+/*computing gcd using repetitive subtraction method*/
+
+#include<stdio.h>
+#include<conio.h>
+int gcd(int m,int n)
+{   int rem;
+    while(n!=0)
+    {
+        rem=m%n;
+        m=n;
+        n=rem;
+    }
+    return m;
+}
+int main()
+{   
+    int m,n;
+    printf("enter the values of m and n\n");
+    scanf("%d%d",&m,&n);
+    m= gcd(m,n);
+    printf("gcd of two numbers is %d\n",m);
+    return 0;
+}
